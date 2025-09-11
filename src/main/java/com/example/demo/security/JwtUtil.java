c883e08b16f8fb2a20c7fad85f99351c20635b09
Thiyara Al Mawaddah <thiyara.mawaddah@28resources.com>
2025-09-11 18:01:29 +0700

    update

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -9,7 +9,8 @@ import javax.crypto.SecretKey;
 public class JwtUtil {
     private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor(
             "my-super-secret-key-which-should-be-long".getBytes());
-    private static final long EXPIRATION_MS = 1000L * 60 * 30;
+
+    private static final long EXPIRATION_MS = 1000L * 60 * 1;
 
     public static String generateToken(String roomId) {
         long now = System.currentTimeMillis();
